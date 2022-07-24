9498ef06e27a4fe3e6401cd3b3f19a1860bfb3a4
MatsuuraDev <108231320+MatsuuraDev@users.noreply.github.com>
2022-07-25 00:46:30 +0700

    Fix for error in build

--- blaze_apollo.mk
+++ blaze_apollo.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 WITH_GAPPS := false
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
